0526ffba180374cc558d35dc6d356a9e70bc2ac5
Cato O. Strand <olsenstrand@gmail.com>
2013-09-23 08:38:00 +0200

    Initialize Repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailstutOne::Application.config.secret_key_base = 'bd64889a5310d1a4592d5a928ce6eb8614b0e686f124c8620d93e67d308cca57b16e01be69f57980eda27d4a4712c87cecaca407293cc91744bd22a2be67c27a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailstutOne::Application.config.session_store :cookie_store, key: '_railstut_one_session'
